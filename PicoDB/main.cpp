9b37a2e9ba797185ab017011d4965678a8b25b6f
Mahmoud Taabodi <mahmoudtaabodi@Mahmouds-Mac-mini.local>
2014-03-26 15:09:35 -0400

    Initial Commit

---
+++ PicoDB/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  PicoDB
+//
+//  Created by Mahmoud Taabodi on 3/26/14.
+//  Copyright (c) 2014 Mahmoud Taabodi. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
