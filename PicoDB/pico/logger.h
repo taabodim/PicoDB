9a9e84c22a4922898ea4aa09be6116bb6f2f5b1b
mtaabodi <taabodim@users.noreply.github.com>
2014-04-10 18:02:20 -0400

    Create logger.h

---
+++ PicoDB/pico/logger.h
@@ -0,0 +1,61 @@
+//
+//  Logger.h
+//  PicoDB
+//
+//  Created by Mahmoud Taabodi on 4/2/14.
+//  Copyright (c) 2014 Mahmoud Taabodi. All rights reserved.
+//
+
+#ifndef PicoDB_Logger_h
+#define PicoDB_Logger_h
+#include <boost/thread.hpp>
+#include <boost/thread/mutex.hpp>
+#include <boost/interprocess/sync/scoped_lock.hpp>
+#include <boost/any.hpp>
+#include <fstream>
+#include <iostream>
+using namespace std;
+namespace pico{
+    
+class logger{
+    
+public:
+    std::ofstream outfile;
+    boost::mutex log_mutex;
+    void log(std::string str )
+    {
+        boost::interprocess::scoped_lock<boost::mutex> lock(log_mutex);
+        std::cout<<"logger : writing the log to file....\n";
+        append(str);
+    }
+    void append(std::string str)
+    {
+        outfile.write((char*) str.c_str(), str.size());
+        outfile.flush();
+    }
+    
+    logger(std::string filename){
+        
+        string path("/Users/mahmoudtaabodi/Documents/");
+		std::string ext(".log");
+		path.append(filename);
+        path.append(ext);
+
+        
+        outfile.open(path,
+                     std::fstream::out | std::fstream::app | std::fstream::binary);
+        std::cout<<"logger instance created...\n";
+    }
+    logger()=delete;
+//    logger operator=(logger& r){
+//        std::cout<<"assignment operator logger instance called..\n";
+//        return r;
+//    
+//    };
+   
+  
+    
+};
+}
+
+#endif
