769538a3f35c07fb20c33ce44ef75936229817f3
luckysurvivor <luckysurvivortwitter@gmail.com>
2014-04-21 20:46:17 -0400

    some todos in future

---
+++ PicoDB/pico/pico_configs.h
@@ -0,0 +1,14 @@
+//
+//  pico_configs.h
+//  PicoDB
+//
+//  Created by Mahmoud Taabodi on 4/21/14.
+//  Copyright (c) 2014 Mahmoud Taabodi. All rights reserved.
+//
+
+#ifndef PicoDB_pico_configs_h
+#define PicoDB_pico_configs_h
+
+
+
+#endif
