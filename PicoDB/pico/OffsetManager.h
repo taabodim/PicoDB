d7759ef6d28cad2149ff654253f632e20c75a28e
taabodim <taabodim@gmail.com>
2014-05-12 17:20:47 -0400

    fix the offset manager later

--- PicoDB/pico/OffsetManager.h
+++ PicoDB/pico/OffsetManager.h
@@ -8,17 +8,27 @@
 
 #ifndef PicoDB_OffsetManager_h
 #define PicoDB_OffsetManager_h
+#include <pico/pico_utils.h>
+#include <atomic>
 namespace pico {
- 
-    class OffsetManager{
-        private :
-        
-        
-        public :
-        
-        
-    };
-}
 
+class OffsetManager: public pico_logger_wrapper {
+private:
+
+	std::atomic<offsetType> offset(0);
+public:
+
+	offsetType getEndOfFileOffset() {
+
+		offsetType x = offset.load(std::memory_order_relaxed);  // get value atomically
+		return x;
+	}
+
+	void setEndOfFileOffset(offsetType x) {
+		offset.store(x, std::memory_order_relaxed);     // set value atomically
+	}
+
+};
+}
 
 #endif
